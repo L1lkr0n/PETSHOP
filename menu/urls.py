8da0c667920f46b5397da95275b6080af8439172
L1lkr0n <alej.martinez@duocuc.cl>
2023-05-18 20:01:58 -0400

    views urls puestas

--- menu/urls.py
+++ menu/urls.py
@@ -16,8 +16,55 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import uno
+from .views import uno,dos,tres,cuatro,cinco,carrodecompra,api,InicioSesion,m_contraseña,r_contraseña,registrarusuario,v_perfilusuario
+from .views import add_newaccesorios,inicio_sesion_admin,delete_accesorios,listar_accesorios,modify_accesorios
+from .views import b_gatoceleste,b_gatocuadrille,b_gatofifi,b_gatolocura,b_gatostreet,b_perrocuadrille,b_perrorayaslocas,b_perrotrajelegante5,b_perrotrajelegante7
+from .views import c_gatoazul,c_gatocarreras,c_gatofreestyle,c_gatosnvalentin,c_gatostreet,c_perroblanco,c_perrocafe,c_perrorojo,c_perrovainilla
+from .views import cr_gatoazul,cr_gatocuadrilles,cr_gatofreestyle,cr_gatonegra,cr_gatorosada,cr_perroazul,cr_perronegra,cr_perronegracadena,cr_perrorojo
 
 urlpatterns = [
     path('',uno,name="uno"),
+    path('dos',dos,name="dos"),
+    path('tres',tres,name="tres"),
+    path('cuatro',cuatro,name="cuatro"),
+    path('cinco',cinco,name="cinco"),
+    path('carrodecompra',carrodecompra,name="carrodecompra"),
+    path('api',api,name="api"),
+    path('InicioSesion',InicioSesion,name="InicioSesion"),
+    path('m_contraseña',m_contraseña,name="m_contraseña"),
+    path('r_contraseña',r_contraseña,name="r_contraseña"),
+    path('registrarusuario',registrarusuario,name="registrarusuario"),
+    path('v_perfilusuario',v_perfilusuario,name="v_perfilusuario"),
+    path('add_newaccesorios',add_newaccesorios,name="add_newaccesorios"),
+    path('inicio_sesion_admin',inicio_sesion_admin,name="inicio_sesion_admin"),
+    path('delete_accesorios',delete_accesorios,name="delete_accesorios"),
+    path('listar_accesorios',listar_accesorios,name="listar_accesorios"),
+    path('modify_accesorios',modify_accesorios,name="modify_accesorios"),
+    path('b_gatoceleste',b_gatoceleste,name="b_gatoceleste.html"),
+    path('b_gatocuadrille',b_gatocuadrille,name="b_gatocuadrille.html"),
+    path('b_gatofifi',b_gatofifi,name="b_gatofifi.html"),
+    path('b_gatolocura',b_gatolocura,name="b_gatolocura.html"),
+    path('b_gatostreet',b_gatostreet,name="b_gatostreet.html"),
+    path('b_perrocuadrille',b_perrocuadrille,name="b_perrocuadrille.html"),
+    path('b_perrorayaslocas',b_perrorayaslocas,name="b_perrorayaslocas.html"),
+    path('b_perrotrajelegante5',b_perrotrajelegante5,name="b_perrotrajelegante5.html"),
+    path('b_perrotrajelegante7',b_perrotrajelegante7,name="b_perrotrajelegante7.html"),
+    path('c_gatoazul',c_gatoazul,name="c_gatoazul.html"),
+    path('c_gatocarreras',c_gatocarreras,name="c_gatocarreras.html"),
+    path('c_gatofreestyle',c_gatofreestyle,name="c_gatofreestyle.html"),
+    path('c_gatosnvalentin',c_gatosnvalentin,name="c_gatosnvalentin.html"),
+    path('c_gatostreet',c_gatostreet,name="c_gatostreet.html"),
+    path('c_perroblanco',c_perroblanco,name="c_perroblanco.html"),
+    path('c_perrocafe',c_perrocafe,name="c_perrocafe.html"),
+    path('c_perrorojo',c_perrorojo,name="c_perrorojo.html"),
+    path('c_perrovainilla',c_perrovainilla,name="c_perrovainilla.html"),
+    path('cr_gatoazul',cr_gatoazul,name="cr_gatoazul"),
+    path('cr_gatocuadrilles',cr_gatocuadrilles,name="cr_gatocuadrilles"),
+    path('cr_gatofreestyle',cr_gatofreestyle,name="cr_gatofreestyle"),
+    path('cr_gatonegra',cr_gatonegra,name="cr_gatonegra"),
+    path('cr_gatorosada',cr_gatorosada,name="cr_gatorosada"),
+    path('cr_perroazul',cr_perroazul,name="cr_perroazul"),
+    path('cr_perronegra',cr_perronegra,name="cr_perronegra"),
+    path('cr_perronegracadena',cr_perronegracadena,name="cr_perronegracadena"),
+    path('cr_perrorojo',cr_perrorojo,name="cr_perrorojo"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -3,3 +3,132 @@ from django.shortcuts import render
 # Create your views here.
 def uno(request):
     return render(request,'menu/uno.html')
+
+def v_perfilusuario(request):
+    return render(request, 'menu/v_perfilusuario.html')
+
+def tres(request):
+    return render(request,'menu/tres.html')
+
+def registrarusuario(request):
+    return render(request,'menu/registrarusuario.html')
+
+def r_contraseña(request):
+    return render(request,'menu/r_contraseña.html')
+
+def m_contraseña(request):
+    return render(request,'menu/m_contraseña.html')
+
+def InicioSesion(request):
+    return render(request,'menu/InicioSesion.html')
+
+def dos(request):
+    return render(request,'menu/dos.html')
+
+def cuatro(request):
+    return render(request,'menu/cuatro.html')
+
+def cinco(request):
+    return render(request,'menu/cinco.html')
+
+def api(request):
+    return render(request,'menu/api.html')
+
+def carrodecompra(request):
+    return render(request, 'menu/carrodecompra.html')
+
+def b_gatoceleste(request):
+    return render(request,'menu/b_gatoceleste.html')
+
+def b_gatocuadrille(request):
+    return render(request,'menu/b_gatocuadrille.html')
+
+def b_gatofifi(request):
+    return render(request,'menu/b_gatofifi.html')
+
+def b_gatolocura(request):
+    return render(request,'menu/b_gatolocura.html')
+
+def b_gatostreet(request):
+    return render(request,'menu/b_gatostreet.html')
+
+def b_perrocuadrille(request):
+    return render(request,'menu/b_perrocuadrille.html')
+
+def b_perrorayaslocas(request):
+    return render(request,'menu/b_perrorayaslocas.html')
+
+def b_perrotrajelegante5(request):
+    return render(request,'menu/b_perroelegante5.html')
+
+def b_perrotrajelegante7(request):
+    return render(request,'menu/b_perroelegante7.html')
+
+def c_gatoazul(request):
+    return render(request,'menu/c_gatoazul.html')
+
+def c_gatocarreras(request):
+    return render(request,'menu/c_gatocarreras.html')
+
+def c_gatofreestyle(request):
+    return render(request,'menu/c_gatofreestyle.html')
+
+def c_gatosnvalentin(request):
+    return render(request,'menu/c_gatosnvalentin.html')
+
+def c_gatostreet(request):
+    return render(request,'menu/c_gatostreet.html')
+
+def c_perroblanco(request):
+    return render(request,'menu/c_perroblanco.html')
+
+def c_perrocafe(request):
+    return render(request,'menu/c_perrocafe.html')
+
+def c_perrorojo(request):
+    return render(request,'menu/c_perrorojo.html')
+
+def c_perrovainilla(request):
+    return render(request,'menu/c_perrovainilla.html')
+
+def cr_gatoazul(request):
+    return render(request,'menu/cr_gatoazul.html')
+
+def cr_gatocuadrilles(request):
+    return render(request,'menu/cr_gatocuadrilles.html')
+
+def cr_gatofreestyle(request):
+    return render(request,'menu/cr_gatofreestyle.html')
+
+def cr_gatonegra(request):
+    return render(request,'menu/cr_gatonegra.html')
+
+def cr_gatorosada(request):
+    return render(request,'menu/cr_gatorosada.html')
+
+def cr_perroazul(request):
+    return render(request,'menu/cr_perroazul.html')
+
+def cr_perronegra(request):
+    return render(request,'menu/cr_perronegra.html')
+
+def cr_perronegracadena(request):
+    return render(request,'menu/cr_perronegracadena.html')
+
+def cr_perrorojo(request):
+    return render(request,'menu/cr_perrorojo.html')
+
+def modify_accesorios(request):
+    return render(request, 'menu/modify_accesorios.html')
+
+def listar_accesorios(request):
+    return render(request, 'menu/listar_accesorios.html')
+
+def inicio_sesion_admin(request):
+    return render(request, 'menu/inicio_sesion_admin.html')
+
+def delete_accesorios(request):
+    return render(request, 'menu/delete_accesorios.html')
+
+def add_newaccesorios(request):
+    return render(request, 'menu/add_newaccesorios.html')
