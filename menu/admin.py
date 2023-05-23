14be853301c2a48fca6d66c185a4cf5669a512fe
TheDarkCitoHD <127221509+TheDarkCitoHD@users.noreply.github.com>
2023-05-23 12:32:59 -0400

    Base Datos (TABLAS 2)

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import models, Region, Comuna, Direccion, Compra, Detalles, Rol, Preguntas, Productos, Categoria, Tipo
 
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Compra)
+admin.site.register(Detalles)
+admin.site.register(Rol)
+admin.site.register(Preguntas)
+admin.site.register(Productos)
+admin.site.register(Categoria)
+admin.site.register(Tipo)
