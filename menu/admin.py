4a020d49c8713cc72e1288a5a998f64bb9747794
L1lkr0n <alej.martinez@duocuc.cl>
2023-05-24 17:25:58 -0400

    url html

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Region,Comuna,Direccion,Usuario,Compra,Detalles,Rol,Preguntas,Producto,Categoria,Tipo
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Usuario)
+admin.site.register(Compra)
+admin.site.register(Detalles)
+admin.site.register(Rol)
+admin.site.register(Preguntas)
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Tipo)
