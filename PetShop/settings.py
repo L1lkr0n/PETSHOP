4213abcaf7707d8d41a69ebc739a0d1d6ff74c9a
L1lkr0n <alej.martinez@duocuc.cl>
2023-06-08 11:32:09 -0400

    mejora de click en productos

--- PetShop/settings.py
+++ PetShop/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'PetShop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xe',
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'petshop3',
         'PASSWORD': '123456789',
         'TEST': {
