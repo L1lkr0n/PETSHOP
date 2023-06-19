e82df6650359590ca25d94ce33b42b19603ea892
L1lkr0n <alej.martinez@duocuc.cl>
2023-06-19 16:35:45 -0400

    modificacion url de api segunda

--- PetShop/urls.py
+++ PetShop/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('menu.urls')),
     path('api/',include('api_noticias.urls')),
-    path('segunda_api/',include('segunda_api.urls')),
+    path('apipreguntas/',include('segunda_api.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
