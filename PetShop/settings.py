ca482304671ac52d66597357bd184b246fa63823
L1lkr0n <alej.martinez@duocuc.cl>
2023-07-10 22:43:28 -0400

    petshop 5 en xe

--- PetShop/settings.py
+++ PetShop/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/xe',
-        'USER': 'petshop4',
+        'USER': 'petshop5',
         'PASSWORD': '123456789',
         'TEST': {
             'USER': 'default_test',
